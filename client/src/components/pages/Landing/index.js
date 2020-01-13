f8ff1a2efc4a8ee407d03d67167954587e39a888
Matt Juskiw <juskiwmatt@gmail.com>
2020-01-13 14:40:32 -0500

    Initialized pages dir

---
+++ client/src/components/pages/Landing/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './style.css';
+
+function Landing() {
+
+}
+
+export default Landing;
---
+++ client/src/components/pages/Portfolio/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './style.css';
+
+function Portfolio() {
+
+}
+
+export default Portfolio;
